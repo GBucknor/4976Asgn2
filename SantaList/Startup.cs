904c01c71d5d09fe5d4fc97fb65fe88905667072
Garel Bucknor <garelbucknor@gmail.com>
2018-11-18 14:49:15 -0800

    Delete extra Jwt Issuer

--- SantaList/Startup.cs
+++ SantaList/Startup.cs
@@ -25,15 +25,6 @@ namespace SantaList
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddCors(options =>
-            {
-                options.AddPolicy("CorsPolicy",
-                    builder => builder.AllowAnyOrigin()
-                    .AllowAnyMethod()
-                    .AllowAnyHeader()
-                    .AllowCredentials());
-            });
-
             services.AddDbContext<SantaContext>(
                 options => options
                 .UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
